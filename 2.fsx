3c75a3e02f2c9c50a7e031719b855d475e78594f
William Bergqvist <shazmodan@gmail.com>
2020-12-21 19:22:29 +0100

    2

---
+++ 2.fsx
@@ -0,0 +1,42 @@
+open System.Text.RegularExpressions
+
+// let input = [
+//     "1-3 a: abcde"
+//     "1-3 b: cdefg"
+//     "2-9 c: ccccccccc"
+// ]
+
+let input =
+    System.IO.File.ReadLines("2.txt")
+    |> Seq.toList
+
+let inputToQuadruple (input: string list) : (int * int * char * string) list =
+    let interpretRow (row: string) =
+        (row, "(\d\d?)-(\d\d?)\s([a-z]):\s(.*)")
+        |> Regex.Match
+        |> (fun regexMatch ->
+            if regexMatch.Success
+            then
+                let min = regexMatch.Groups.[1].Value |> int
+                let max = regexMatch.Groups.[2].Value |> int
+                let char = regexMatch.Groups.[3].Value |> char
+                let password = regexMatch.Groups.[4].Value
+                min, max, char, password
+            else failwith "Could not regexMatch correctly.")
+    
+    List.map interpretRow input
+
+let isValidPassword (min: int) (max: int) (char: char) (password: string) : bool =
+    let occurances = 
+        password
+        |> String.filter (fun s -> s = char)
+        |> String.length
+    
+    occurances >= min && occurances <= max
+
+
+input
+|> inputToQuadruple
+|> List.map (fun (min, max, char, password) -> isValidPassword min max char password)
+|> List.filter id
+|> List.length
