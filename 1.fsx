2da8732764128d563c6ebba7218094f0688ecc20
William Bergqvist <shazmodan@gmail.com>
2020-12-21 18:13:02 +0100

    1-2

--- 1.fsx
+++ 1.fsx
@@ -9,10 +9,23 @@ let rec pairs (numbers: int list) : (int * int) list =
     | head :: tail ->
         tail 
         |> List.map (fun number -> (head, number))
-        |> (fun tuples -> tuples @ pairs tail)
+        |> fun tuples -> tuples @ pairs tail
 
+let rec triples (numbers: int list) : (int * int * int) list =
+    match numbers with
+    | [] -> []
+    | head :: tail ->
+        pairs numbers
+        |> List.map (fun (x, y) -> (head, x, y))
+        |> fun threes -> threes @ triples tail
 
 numbers
 |> pairs
 |> List.find (fun (a,b) -> a + b = 2020)
 |> fun (a,b) -> a * b
+
+
+numbers
+|> triples
+|> List.find (fun (a,b,c) -> a + b + c = 2020)
+|> fun (a,b,c) -> a * b * c
